96256d5fb532ff7c966eab02973467e74dd3fa21
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-03-26 01:04:29 +0500

    Updated Widget

--- lib/widgets/category_screen.dart
+++ lib/widgets/category_screen.dart
@@ -3,34 +3,40 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CategoryScreen extends StatelessWidget {
   final String label;
+  final IconData icon;
   final bool isSelected;
   final VoidCallback onSelected;
 
   const CategoryScreen({
     super.key,
     required this.label,
+    required this.icon,
     required this.isSelected,
     required this.onSelected,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 8),
-      child: ChoiceChip(
-        label: Text(label),
-        selected: isSelected,
-        onSelected: (_) => onSelected(),
-        selectedColor: const Color(0xFF1A237E),
-        labelStyle: GoogleFonts.workSans(
-          color: isSelected ? Colors.white : const Color(0xFF1A237E),
-          fontWeight: FontWeight.w600,
-        ),
-        backgroundColor: const Color(0xFFF2F3FF),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        side: BorderSide.none,
-        showCheckmark: false,
+    return ChoiceChip(
+      avatar: Icon(
+        icon,
+        size: 18,
+        color: isSelected ? Colors.white : const Color(0xFF000666),
       ),
+      label: Text(label.toUpperCase()),
+      selected: isSelected,
+      onSelected: (_) => onSelected(),
+      selectedColor: const Color(0xFF000666),
+      labelStyle: GoogleFonts.workSans(
+        color: isSelected ? Colors.white : const Color(0xFF000666),
+        fontWeight: FontWeight.bold,
+        fontSize: 12,
+      ),
+      backgroundColor: const Color(0xFFF2F3FF),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      side: BorderSide.none,
+      showCheckmark: false,
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
     );
   }
 }
